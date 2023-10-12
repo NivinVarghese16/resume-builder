a5a832a5e105c6829eeade034b9e3a0549970481
NivinVarghese16 <nivinvarghese16@gmail.com>
2023-10-12 16:05:57 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center">Generate your Resume!</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
